ebeebe3ccbdeb1420c8263b76928b226faa85766
wnbo9 <wnbo@umich.edu>
2024-11-08 15:33:39 -0500

    package

---
+++ dap/R/hello.R
@@ -0,0 +1,11 @@
+#' Say Hello
+#'
+#' This function prints "Hello, world!".
+#'
+#' @return A character string "Hello, world!".
+#' @export
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
