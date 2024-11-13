43646f0ac55fb048cd4a28ef35a233f3451094bd
wnbo9 <wnbo@umich.edu>
2024-11-13 10:45:11 -0500

    rcpp

--- dap/R/hello.R
+++ dap/R/hello.R
@@ -1,11 +1,21 @@
-#' Say Hello
-#'
-#' This function prints "Hello, world!".
-#'
-#' @return A character string "Hello, world!".
+#' Test function
 #' @export
-#' @examples
-#' hello()
 hello <- function() {
   print("Hello, world!")
+}
+
+#' DAP-PIR
+#' @param X Genotype data
+#' @return Fine mapping results
+#' @export
+dap <- function(X=NULL) {
+  print('using dap...')
+
+  # load data
+
+  # pseudo importance resampling
+
+  # deterministic approximation of posteriors
+
+  # results
 }
